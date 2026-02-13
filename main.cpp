947faf0a2a18c0a2ce5474c599f1e41cf641f703
MM_EE <mmeeprogram@gmail.com>
2026-02-14 02:58:46 +0900

    first commit
    20分で作った！

---
+++ main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string to_double_number(int i) {
+	if (i < 10) {
+		return "0" + to_string(i);
+	}
+	else {
+		return to_string(i);
+	}
+}
+
+bool is_prime(string str_mmdd) {
+	int mmdd = stoi(str_mmdd);
+	if (mmdd < 2) return false;
+
+	for (int i = 2; i < mmdd; i++) {
+		if (mmdd / i * i == mmdd) return false; // int / intは切り捨てだから、割り切れたら約数ってことになる！
+	}
+
+	return true;
+}
+
+int main() {
+	string max_days = "121010110101"; // 0が30日、1が31日、2が29日まで！
+	int result = 0;
+
+	for (int i = 0; i < max_days.length(); i++) {
+		int month = i + 1;
+		string str_month = to_double_number(month);
+
+		int max_day;
+		if (max_days[i] == '0') max_day = 30;
+		if (max_days[i] == '1') max_day = 31;
+		if (max_days[i] == '2') max_day = 29;
+
+		for (int j = 0; j < max_day; j++) {
+			int day = j + 1;
+			string str_day = to_double_number(day);
+
+			if (is_prime(str_month + str_day)) {
+				result++;
+
+				cout << result << ": " << str_month << "/" << str_day << endl;
+			}
+		}
+	}
+
+	cout << "result: " << result << endl;
+}
